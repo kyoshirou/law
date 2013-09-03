44b8bacdf7993b8bb3100801f6c8b6af4defd219
kyoshirou <kyoshirou@gmail.com>
2013-09-03 15:37:29 +0800

    Create al-packer.sh

---
+++ al-packer.sh
@@ -0,0 +1,1 @@
+sudo pacman -S wget --noconfirm && wget https://aur.archlinux.org/packages/pa/packer/packer.tar.gz && tar -xvzf packer.tar.gz && cd packer && makepkg -s --asroot && pacman -U *.xz  --noconfirm
