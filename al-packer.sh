df3938abbd2882f68ab40ffef5ec2126d08e982c
kyoshirou <kyoshirou@gmail.com>
2013-09-24 01:27:39 +0800

    Update al-packer.sh

--- al-packer.sh
+++ al-packer.sh
@@ -1,1 +1,6 @@
-sudo pacman -S wget --noconfirm && wget https://aur.archlinux.org/packages/pa/packer/packer.tar.gz && tar -xvzf packer.tar.gz && cd packer && makepkg -s --asroot && pacman -U *.xz  --noconfirm
+sudo pacman -S wget --noconfirm
+wget https://aur.archlinux.org/packages/pa/packer/packer.tar.gz
+tar -xvzf packer.tar.gz
+cd packer
+makepkg -s --asroot
+pacman -U *.xz  --noconfirm
