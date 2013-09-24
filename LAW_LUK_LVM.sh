743ae915b68138ac03a60b137428657de59a4772
kyoshirou <kyoshirou@gmail.com>
2013-09-25 05:30:07 +0800

    Update LAW_LUK_LVM.sh

--- LAW_LUK_LVM.sh
+++ LAW_LUK_LVM.sh
@@ -38,51 +38,6 @@ modprobe efivars
 ls -1 /sys/firmware/efi/vars/  
 clear 
 echo Changing root to the newly installed Archlinuz system 
-#arch-chroot /mnt
+arch-chroot /mnt
 
-arch_chroot clear 
-arch_chroot echo Enter the Hostname for the new ArchLinux System: 
-arch_chroot read hostname 
-arch_chroot echo $hostname > /etc/hostname 
-arch_chroot clear 
-arch_chroot echo Enter the root\'s password for the new system: 
-arch_chroot passwd 
-arch_chroot clear 
-arch_chroot echo Enter a user name for the new system: 
-arch_chroot read usr 
-arch_chroot usr=$(echo $usr | tr '[A-Z]' '[a-z]') useradd -m -g users -G wheel,audio,video,storage,power -s /bin/bash $usr 
-arch_chroot clear 
-arch_chroot as=\'s 
-arch_chroot echo Enter the $usr$as password for the new system: 
-arch_chroot passwd $usr 
-arch_chroot sed -i '96a\[multilib]\n\SigLevel = PackageRequired\nInclude = /etc/pacman.d/mirrorlist\n' /etc/pacman.conf 
-arch_chroot pacman -Syy 
-arch_chroot sed -i  '1i\en_US.UTF-8 UTF-8' /etc/locale.gen 
-arch_chroot locale-gen 
-arch_chroot echo LANG=en_US.UTF-8 > /etc/locale.conf 
-arch_chroot export LANG=en_US.UTF-8 
-arch_chroot ln -s /usr/share/zoneinfo/Singapore /etc/localtime 
-arch_chroot hwclock --systohc --utc 
-arch_chroot pacman -S networkmanager network-manager-applet --noconfirm 
-arch_chroot systemctl enable NetworkManager.service 
-arch_chroot sed -i  's@autodetect modconf block@autodetect modconf block encrypt lvm2 @g' /etc/mkinitcpio.conf 
-arch_chroot mkinitcpio -p linux 
-arch_chroot pacman -S xorg-server xorg-xinit xorg-server-utils virtualbox-guest-utils xfce4 xfce4-goodies firefox lxdm --noconfirm 
-arch_chroot systemctl enable lxdm.service 
-arch_chroot sed -i '/# session=\/usr\/bin\/startlxde/a\\nsession=\/usr\/bin\/startxfce4\nautologin='$usr'' /etc/lxdm/lxdm.conf 
-arch_chroot echo -e "vboxguest\nvboxsf\nvboxvideo" > /etc/modules-load.d/virtualbox.conf 
-arch_chroot pacman -S grub-efi-x86_64 efibootmgr --noconfirm 
-arch_chroot grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck 
-arch_chroot cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo ID=$(df /boot/efi/EFI |egrep -o /dev/sd'[a-z][0-9]' | sed 's/[0-9]*//g') 
-arch_chroot DN=$(df /boot/efi/EFI |egrep -o /dev/sd'[a-z][0-9]' | sed 's/[/a-z]*//g') 
-arch_chroot efibootmgr -c -g -d $ID -p $DN -w -L "Arch Linux (GRUB)" -l '\EFI\arch_grub\grubx64.efi'
-arch_chroot grub-mkconfig -o /boot/grub/grub.cfg 
-arch_chroot RO=$(blkid | grep crypto_LUKS | egrep -o /dev/sd'[a-z][0-99]') 
-arch_chroot sed -i 's@/vmlinuz-linux@/vmlinuz-linux cryptdevice='$RO':ArchSysLuks @g' /boot/grub/grub.cfg 
-arch_chroot mkdir -p /boot/efi/EFI/boot 
-arch_chroot cp /boot/efi/EFI/arch_grub/grubx64.efi /boot/efi/EFI/boot/bootx64.efi
-arch_chroot clear
-arch_chroot clear 
-arch_chroot echo -e "The new Archlinux system installation is completed. Please Reboot ;)"
-exit
 
