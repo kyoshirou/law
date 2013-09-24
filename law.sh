2b64631e08503d05a0d5125a83cec9fdb9e3a00b
kyoshirou <kyoshirou@gmail.com>
2013-09-25 06:56:27 +0800

    Update law.sh

--- law.sh
+++ law.sh
@@ -30,44 +30,45 @@ pacstrap /mnt base base-devel
 genfstab -U -p /mnt >> /mnt/etc/fstab
 sed -i  's/codepage=cp437/codepage=437/' /mnt/etc/fstab
 sed -i  's/,data=ordered//' /mnt/etc/fstab
-@arch-chroot /mnt
+#arch-chroot /mnt
 
 clear
-arch-chroot "echo Enter the Hostname for the new ArchLinux System:"
-arch-chroot "read hostname"
-arch-chroot "echo $hostname > /mnt/etc/hostname"
+arch_chroot "echo Enter the Hostname for the new ArchLinux System:"
+arch_chroot "read hostname"
+arch_chroot "echo $hostname > /mnt/etc/hostname"
 clear
-arch-chroot "echo Enter the root\'s password for the new system:"
-arch-chroot "passwd"
+arch_chroot "echo Enter the root\'s password for the new system:"
+arch_chroot "passwd"
 clear
-arch-chroot "echo Enter a user name for the new system:"
-arch-chroot "read usr"
-arch-chroot "usr=$(echo $usr | tr '[A-Z]' '[a-z]')"
-arch-chroot "useradd -m -g users -G wheel,audio,video,storage,power -s /bin/bash $usr"
+arch_chroot "echo Enter a user name for the new system:"
+arch_chroot "read usr"
+arch_chroot "usr=$(echo $usr | tr '[A-Z]' '[a-z]')"
+arch_chroot "useradd -m -g users -G wheel,audio,video,storage,power -s /bin/bash $usr"
 clear
-arch-chroot "as=\'s"
-arch-chroot "echo Enter the $usr$as password for the new system:"
-arch-chroot "passwd $usr"
-arch-chroot "sed -i '96a\[multilib]\n\SigLevel = PackageRequired\nInclude = /etc/pacman.d/mirrorlist\n' /mnt/etc/pacman.conf"
-arch-chroot "pacman -Syy"
-arch-chroot "sed -i  '1i\en_US.UTF-8 UTF-8' /mnt/etc/locale.ge"
-arch-chroot "locale-gen"
-arch-chroot "echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf"
-arch-chroot "export LANG=en_US.UTF-8"
-arch-chroot "ln -s /usr/share/zoneinfo/Singapore /mnt/etc/localtime"
-arch-chroot "hwclock --systohc --utc"
-arch-chroot "pacman -S networkmanager network-manager-applet --noconfirm"
-arch-chroot "systemctl enable NetworkManager.service"
-arch-chroot "sed -i  's@autodetect modconf block@autodetect modconf block encrypt lvm2 @g' /mnt/etc/mkinitcpio.conf"
-arch-chroot "mkinitcpio -p linux"
-arch-chroot "pacman -S xorg-server xorg-xinit xorg-server-utils --noconfirm"
-arch-chroot "pacman -S grub-bios --noconfirm"
-arch-chroot "grub-install --target=i386-pc --recheck /dev/sda"
-arch-chroot "mkdir -p /mnt/boot/grub/locale"
-arch-chroot "grub-mkconfig -o /mnt/boot/grub/grub.cfg"
-arch-chroot "sed -i 's@/vmlinuz-linux@/vmlinuz-linux cryptdevice=/dev/sda3:ArchSysLuks@g' /mnt/boot/grub/grub.cfg"
+arch_chroot "as=\'s"
+arch_chroot "echo Enter the $usr$as password for the new system:"
+arch_chroot "passwd $usr"
+arch_chroot "sed -i '96a\[multilib]\n\SigLevel = PackageRequired\nInclude = /etc/pacman.d/mirrorlist\n' /mnt/etc/pacman.conf"
+arch_chroot "pacman -Syy"
+arch_chroot "sed -i  '1i\en_US.UTF-8 UTF-8' /mnt/etc/locale.ge"
+arch_chroot "locale-gen"
+arch_chroot "echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf"
+arch_chroot "export LANG=en_US.UTF-8"
+arch_chroot "ln -s /usr/share/zoneinfo/Singapore /mnt/etc/localtime"
+arch_chroot "hwclock --systohc --utc"
+arch_chroot "pacman -S networkmanager network-manager-applet --noconfirm"
+arch_chroot "systemctl enable NetworkManager.service"
+arch_chroot "sed -i  's@autodetect modconf block@autodetect modconf block encrypt lvm2 @g' /mnt/etc/mkinitcpio.conf"
+arch_chroot "mkinitcpio -p linux"
+arch_chroot "pacman -S xorg-server xorg-xinit xorg-server-utils --noconfirm"
+arch_chroot "pacman -S grub-bios --noconfirm"
+arch_chroot "grub-install --target=i386-pc --recheck /dev/sda"
+arch_chroot "mkdir -p /mnt/boot/grub/locale"
+arch_chroot "grub-mkconfig -o /mnt/boot/grub/grub.cfg"
+arch_chroot "sed -i 's@/vmlinuz-linux@/vmlinuz-linux cryptdevice=/dev/sda3:ArchSysLuks@g' /mnt/boot/grub/grub.cfg"
 clear
-arch-chroot "echo The new Archlinux system installation is completed. Please Reboot"
-arch-chroot "exit"
+arch_chroot "echo The new Archlinux system installation is completed. Please Reboot"
+arch_chroot "exit"
 
 reboot
+
