ae741d0f917a245dd8e81d20985f76b3fac2218e
kyoshirou <kyoshirou@gmail.com>
2013-09-26 05:00:44 +0800

    Update LAW_02.sh

--- LAW_02.sh
+++ LAW_02.sh
@@ -1,9 +1,9 @@
 arch_chroot() { #{{{
     arch-chroot /mnt /bin/bash -c "${1}"
   }
-ID=$(df /mnt/boot/efi/EFI |egrep -o /dev/sd'[a-z][0-9]' | sed 's/[0-9]*//g')
-DN=$(df /mnt/boot/efi/EFI |egrep -o /dev/sd'[a-z][0-9]' | sed 's/[/a-z]*//g')
-arch_chroot "efibootmgr -c -g -d $ID -p $DN -w -L 'Arch Linux (GRUB)' -l '\EFI\arch_grub\grubx64.efi'"
+#ID=$(df /mnt/boot/efi/EFI |egrep -o /dev/sd'[a-z][0-9]' | sed 's/[0-9]*//g')
+#DN=$(df /mnt/boot/efi/EFI |egrep -o /dev/sd'[a-z][0-9]' | sed 's/[/a-z]*//g')
+arch_chroot "efibootmgr -c -g -d /dev/sda -p 1 -w -L 'Arch Linux (GRUB)' -l '\EFI\arch_grub\grubx64.efi'"
 arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
 RO=$(blkid | grep crypto_LUKS | egrep -o /dev/sd'[a-z][0-99]')
 arch_chroot "sed -i 's@/vmlinuz-linux@/vmlinuz-linux cryptdevice='$RO':ArchSysLuks @g' /boot/grub/grub.cfg"
