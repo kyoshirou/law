2ca0ed14508e1b1774b7d1b615880ce39b2d908b
kyoshirou <kyoshirou@gmail.com>
2013-09-26 04:12:10 +0800

    Update LAW_02.sh

--- LAW_02.sh
+++ LAW_02.sh
@@ -1,3 +1,6 @@
+arch_chroot() { #{{{
+    arch-chroot /mnt /bin/bash -c "${1}"
+  }
 ID=$(df /mnt/boot/efi/EFI |egrep -o /dev/sd'[a-z][0-9]' | sed 's/[0-9]*//g')
 DN=$(df /mnt/boot/efi/EFI |egrep -o /dev/sd'[a-z][0-9]' | sed 's/[/a-z]*//g')
 arch_chroot "efibootmgr -c -g -d $ID -p $DN -w -L 'Arch Linux (GRUB)' -l '\EFI\arch_grub\grubx64.efi'"
@@ -6,7 +9,6 @@ RO=$(blkid | grep crypto_LUKS | egrep -o /dev/sd'[a-z][0-99]')
 arch_chroot "sed -i 's@/vmlinuz-linux@/vmlinuz-linux cryptdevice='$RO':ArchSysLuks @g' /boot/grub/grub.cfg"
 arch_chroot "mkdir -p /boot/efi/EFI/boot"
 arch_chroot "cp /boot/efi/EFI/arch_grub/grubx64.efi /boot/efi/EFI/boot/bootx64.efi"
-clear
-clear 
-arch_chroot "echo -e "The new Archlinux system installation is completed. Please Reboot ;)""
+
+arch_chroot "echo -e 'The new Archlinux system installation is completed. Please Reboot ;)'"
 exit
