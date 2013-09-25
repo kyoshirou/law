0227cb260127942de29e7966164d3e865d02f901
kyoshirou <kyoshirou@gmail.com>
2013-09-26 02:40:44 +0800

    Update law.sh

--- law.sh
+++ law.sh
@@ -25,6 +25,7 @@ mkdir /mnt/home
 mount /dev/ArchSys/home /mnt/home
 mkdir /mnt/boot
 mount /dev/sda2 /mnt/boot
+sed -i  '1i\Server = http://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
 sed -i  '1i\Server = http://mirror.nus.edu.sg/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel
 genfstab -U -p /mnt >> /mnt/etc/fstab
