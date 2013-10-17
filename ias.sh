bf30cd6a1b7c52b30284a91ca4d9d090606fd90f
kyoshirou <kyoshirou@gmail.com>
2013-10-18 05:29:40 +0800

    Update ias.sh

--- ias.sh
+++ ias.sh
@@ -1,31 +1,50 @@
-sudo packer -S 
-ttf-ms-fonts \
-ttf-tw \
-wqy-microhei \
-wqy-zenhei \
+sudo packer -S  \
+lib32-sdl_ttf  \
+opendesktop-fonts  \
+sdl_ttf  \
 ttf-arphic-ukai  \
 ttf-arphic-uming  \
-opendesktop-fonts  \
-wqy-bitmapfont  \
-ttf-hannom  \
-sdl_ttf  \
 ttf-bitstream-vera  \
-ttf-freefont  \
-ttf-linux-libertine  \
-ttf-sazanami  \
 ttf-droid  \
+ttf-freefont  \
 ttf-gentium  \
 ttf-hanazono  \
+ttf-hannom  \
 ttf-inconsolata  \
 ttf-liberation  \
-lib32-sdl_ttf  \
+ttf-linux-libertine  \
+ttf-ms-fonts \
+ttf-sazanami  \
+ttf-tw \
 ttf-ubuntu-font-family \
-#
-archey3 firefox google-chrome-beta chromium icedtea-web-java7 easystroke flashplugin python bleachbit conky-colors wget evince openssl098 \
-gigolo uget aria2 iptables dnsmasq openresolv samba gvfs-smb \
+wqy-bitmapfont  \
+wqy-microhei \
+wqy-zenhei \
+archey \
+firefox  \
+google-chrome-beta  \
+chromium  \
+icedtea-web-java7  \
+easystroke  \
+flashplugin  \
+bleachbit  \
+conky-colors  \
+evince  \
+openssl098 \
+gigolo  \
+uget  \
+aria2  \
+iptables  \
+dnsmasq  \
+openresolv  \
+samba  \
+gvfs-smb \
 xarchiver tar gzip bzip2 zip unzip unrar p7zip arj xz lzop wxgtk xdg-user-dirs \
-pulseaudio pavucontrol alsa-utils pulseaudio-alsa lib32-alsa-lib lib32-libpulse jack ffmpeg lib32-alsa-plugins lib32-jack lib32-libsamplerate lib32-speex libcanberra-gstreamer \
-smplayer vlc radiotray gstreamer0.10-plugins clementine unrar python2-gobject2 libcec libusb-compat python-dbus flac librsvg vcdimager libcdio ttf-freefont libxv sdl osdlyrics xvba-video \
+pulseaudio pavucontrol alsa-utils pulseaudio-alsa lib32-alsa-lib lib32-libpulse  \
+jack ffmpeg lib32-alsa-plugins lib32-jack lib32-libsamplerate lib32-speex libcanberra-gstreamer \
+smplayer vlc radiotray gstreamer0.10-plugins clementine unrar python2-gobject2 libcec libusb-compat  \
+python-dbus flac librsvg vcdimager libcdio ttf-freefont libxv sdl osdlyrics xvba-video \
 faenza-icon-theme gtk gtk2 gtk3 \
-thunderbird pidgin purple-plugin-pack pidgin-libnotify libcanberra libcanberra-pulse libcanberra-gstreamer geany xterm python python2 \
+thunderbird pidgin purple-plugin-pack pidgin-libnotify libcanberra libcanberra-pulse libcanberra-gstreamer  \
+geany xterm python python2 \
 gparted clonezilla dosfstools ntfs-3g 
