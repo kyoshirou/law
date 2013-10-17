a8c237118ad4420efa54b98d2b5a9b8d497b2a7c
kyoshirou <kyoshirou@gmail.com>
2013-10-18 05:17:21 +0800

    Update ias.sh

--- ias.sh
+++ ias.sh
@@ -1,5 +1,27 @@
-sudo packer -S ttf-ms-fonts ttf-tw wqy-microhei wqy-zenhei ttf-arphic-ukai ttf-arphic-uming opendesktop-fonts wqy-bitmapfont ttf-hannom sdl_ttf ttf-bitstream-vera ttf-freefont ttf-linux-libertine ttf-sazanami ttf-droid ttf-gentium ttf-hanazono ttf-inconsolata ttf-liberation lib32-sdl_ttf ttf-ubuntu-font-family \
-archey firefox google-chrome-beta chromium icedtea-web-java7 easystroke flashplugin python bleachbit conky-colors wget evince openssl098 \
+sudo packer -S 
+ttf-ms-fonts \
+ttf-tw \
+wqy-microhei \
+wqy-zenhei \
+ttf-arphic-ukai  \
+ttf-arphic-uming  \
+opendesktop-fonts  \
+wqy-bitmapfont  \
+ttf-hannom  \
+sdl_ttf  \
+ttf-bitstream-vera  \
+ttf-freefont  \
+ttf-linux-libertine  \
+ttf-sazanami  \
+ttf-droid  \
+ttf-gentium  \
+ttf-hanazono  \
+ttf-inconsolata  \
+ttf-liberation  \
+lib32-sdl_ttf  \
+ttf-ubuntu-font-family \
+#
+archey3 firefox google-chrome-beta chromium icedtea-web-java7 easystroke flashplugin python bleachbit conky-colors wget evince openssl098 \
 gigolo uget aria2 iptables dnsmasq openresolv samba gvfs-smb \
 xarchiver tar gzip bzip2 zip unzip unrar p7zip arj xz lzop wxgtk xdg-user-dirs \
 pulseaudio pavucontrol alsa-utils pulseaudio-alsa lib32-alsa-lib lib32-libpulse jack ffmpeg lib32-alsa-plugins lib32-jack lib32-libsamplerate lib32-speex libcanberra-gstreamer \
