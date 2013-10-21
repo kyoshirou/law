2839782aa2dc1fc92cc97a303f835ec9810265cc
kyoshirou <kyoshirou@gmail.com>
2013-10-22 04:41:32 +0800

    Update vbox.sh

--- vbox.sh
+++ vbox.sh
@@ -3,6 +3,6 @@ arch_chroot() {
   }
 arch_chroot "pacman -S virtualbox-guest-utils xfce4 xfce4-goodies lxdm --noconfirm"
 arch_chroot "systemctl enable lxdm.service"
-read usrx
+#read usrx
 arch_chroot "sed -i '/# session=\/usr\/bin\/startlxde/a\\nsession=\/usr\/bin\/startxfce4\nautologin='$usr'' /etc/lxdm/lxdm.conf"
 arch_chroot "echo -e "vboxguest\nvboxsf\nvboxvideo" > /etc/modules-load.d/virtualbox.conf"
