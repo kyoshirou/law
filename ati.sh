8cff4ba2afd9231d6a8edbf3100dce79dd911bea
kyoshirou <kyoshirou@gmail.com>
2013-10-26 02:08:53 +0800

    Update ati.sh

--- ati.sh
+++ ati.sh
@@ -1,4 +1,4 @@
 arch_chroot() { 
   arch-chroot /mnt /bin/bash -c "${1}"
   }
-arch_chroot "pacman -Sxf86-video-intel xf86-video-ati lib32-ati-dri ttf-dejavu xcalib --noedit --noconfirm"
+arch_chroot "packer -S xf86-video-intel xf86-video-ati lib32-ati-dri ttf-dejavu xcalib --noedit --noconfirm"
