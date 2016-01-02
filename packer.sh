0fb40dceeae7edb2d1994640a0b8aa276de38106
kyoshirou <kyoshirou@gmail.com>
2016-01-03 07:03:45 +0800

    Delete packer.sh

--- packer.sh
+++
@@ -1,9 +0,0 @@
-sudo pacman -S wget --noconfirm
-wget https://aur.archlinux.org/packages/pa/packer/packer.tar.gz
-tar -xvzf packer.tar.gz
-cd packer
-makepkg -s --asroot
-pacman -U *.xz  --noconfirm
-cd ..
-rm -r packer
-rm -f packer*
